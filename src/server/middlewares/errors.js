39c98558910831363f69642c20a119ac1cf9642b
Alejandro Gomez <jandrogo@gmail.com>
2022-05-30 19:32:31 +0200

    Added eslint disable to avoid unused-vars in generalError function

--- src/server/middlewares/errors.js
+++ src/server/middlewares/errors.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const debug = require("debug")("japanthings:server:middlewares:errors");
 const chalk = require("chalk");
 
+// eslint-disable-next-line no-unused-vars
 const generalError = (error, req, res, next) => {
   debug(chalk.red(error.message || error.customMessage));
   const message = error.customMessage ?? "General error";
