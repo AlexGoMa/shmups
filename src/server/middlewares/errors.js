8b71c9906d499a9a1a427f0627464c2e1c79d5fa
Alejandro Gomez <jandrogo@gmail.com>
2022-05-30 19:28:47 +0200

    Added generalError function in errors.js in middlewares

---
+++ src/server/middlewares/errors.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const debug = require("debug")("japanthings:server:middlewares:errors");
+const chalk = require("chalk");
+
+const generalError = (error, req, res, next) => {
+  debug(chalk.red(error.message || error.customMessage));
+  const message = error.customMessage ?? "General error";
+  const statusCode = error.statusCode ?? 500;
+
+  res.status(statusCode).json({ error: true, message });
+};
+
+module.exports = generalError;
