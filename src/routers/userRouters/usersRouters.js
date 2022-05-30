fd45c67e36503aee6539a6c349d75e2566980086
AlexGoMa <97446730+AlexGoMa@users.noreply.github.com>
2022-05-30 21:22:33 +0200

    Revert "Feature/user routers"

--- src/routers/userRouters/usersRouters.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const express = require("express");
-const registerUser = require("../../server/controllers/userControllers/userControllers");
-
-const usersRouters = express.Router();
-
-usersRouters.post("/register", registerUser);
-
-module.exports = usersRouters;
