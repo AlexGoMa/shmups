2c50be5cc743c7cc3349e5b08f31ac339d8c7d04
Alejandro Gomez <jandrogo@gmail.com>
2022-06-07 20:00:35 +0200

    Added createMessageController in messagesControllers

--- src/server/routers/messageRouters/messagesRouters.js
+++ src/server/routers/messageRouters/messagesRouters.js
@@ -3,11 +3,15 @@ const express = require("express");
 const {
   getMessages,
   deleteMessage,
+  createMessage,
 } = require("../../controllers/userControllers/messagesControllers");
+
+const { auth } = require("../../middlewares/auth");
 
 const messagesRouters = express.Router();
 
-messagesRouters.get("/list", getMessages);
+messagesRouters.get("/list", auth, getMessages);
 messagesRouters.delete("/:id", deleteMessage);
+messagesRouters.post("/:id", createMessage);
 
 module.exports = messagesRouters;
