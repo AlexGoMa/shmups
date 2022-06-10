133dde96f2111961809dbe530872370769f7442c
Alejandro Gomez <jandrogo@gmail.com>
2022-06-10 22:53:40 +0200

    Added firebase middleware, not working thought

--- database/models/Message.js
+++ database/models/Message.js
@@ -9,6 +9,10 @@ const MessageSchema = new Schema({
     type: String,
     required: false,
   },
+  // imageBackup: {
+  //  type: String,
+  //  required: false,
+  // },
   category: {
     type: String,
     required: true,
--- src/server/routers/messageRouters/messagesRouters.js
+++ src/server/routers/messageRouters/messagesRouters.js
@@ -13,25 +13,27 @@ const {
 } = require("../../controllers/userControllers/messagesControllers");
 
 const { auth } = require("../../middlewares/auth");
+// const firebase = require("../../middlewares/firebase"); //
 
 const messagesRouters = express.Router();
 
 const upload = multer({
   dest: path.join("uploads", "images"),
   limits: {
-    fieldSize: 1024 * 1024,
+    fieldSize: 1000000,
   },
 });
 
 messagesRouters.get("/list", auth, getMessages);
 messagesRouters.delete("/:id", auth, deleteMessage);
-messagesRouters.get("/:id", auth, getOneMessage);
+messagesRouters.get("/one/:id", auth, getOneMessage);
 
 messagesRouters.get("/mine", auth, getUserMessages);
 messagesRouters.post(
   "/mine/create",
   auth,
   upload.single("image"),
+  // firebase,
   createMessage
 );
 
